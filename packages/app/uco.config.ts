694f390bd5206d2a03472eddd533581313d99492
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2022-10-29 19:40:14 +0900

    fix: fix spinner

--- packages/app/uco.config.ts
+++ packages/app/uco.config.ts
@@ -3,6 +3,10 @@ import transformerDirectives from "@unocss/transformer-directives";
 import { defineConfig } from "unocss";
 
 export default defineConfig({
+  shortcuts: {
+    spinner:
+      "animate-spin rounded-full border-2 border-gray-500 border-t-gray-300 border-l-gray-300",
+  },
   presets: [presetUno()],
   transformers: [transformerDirectives()],
 });
