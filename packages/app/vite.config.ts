70653e55672726da03f13c697d972688c83a497f
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2022-10-30 17:23:23 +0900

    chore: fix unocss config typo

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -1,14 +1,13 @@
 import unocss from "@unocss/vite";
 import rakkas from "rakkasjs/vite-plugin";
 import { defineConfig } from "vite";
-import unocssConfig from "./uco.config";
 
 export default defineConfig({
   base: "./",
   build: {
     sourcemap: true,
   },
-  plugins: [unocss(unocssConfig), rakkas({ adapter: "vercel" })],
+  plugins: [unocss(), rakkas({ adapter: "vercel" })],
   worker: {
     // workaround for iife worker bug with `?url` https://github.com/vitejs/vite/issues/9879
     format: "es",
