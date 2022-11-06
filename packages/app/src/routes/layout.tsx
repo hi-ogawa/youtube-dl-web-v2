a1403b5e4e8ba1fa152376402181563d826e5140
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2022-11-06 14:10:02 +0900

    chore: add comment

--- packages/app/src/routes/layout.tsx
+++ packages/app/src/routes/layout.tsx
@@ -45,6 +45,7 @@ function PageHeader() {
     }
   }, [location.pending]);
 
+  // shadow color taken from https://ant.design/components/overview/
   return (
     <header className="flex items-center gap-3 px-6 py-2 shadow-[0_2px_8px_#f0f1f2] dark:shadow-[0_2px_8px_#000000a6]">
       <button className="p-1 btn btn-ghost" onClick={() => setMenuOpen(true)}>
