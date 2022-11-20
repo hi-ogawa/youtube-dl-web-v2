28d93ef664d4ce9ac7e860620bed034fead36647
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2022-11-20 16:23:43 +0900

    refactor: bump unocss and use aria variant

--- packages/app/uno.config.ts
+++ packages/app/uno.config.ts
@@ -20,23 +20,11 @@ const mapping = themeSrc
 export default defineConfig({
   theme: {
     colors: Object.fromEntries(mapping),
-  },
-  variants: [
-    {
-      // https://github.com/unocss/unocss/blob/f4954d2a2b2a3dc4ad32d1ea098aab07596c55b1/packages/preset-mini/src/_variants/pseudo.ts
-      // e.g. aria-invalid:border-error
-      match: (input) => {
-        const match = input.match(/^(aria-invalid:)/);
-        if (match) {
-          return {
-            matcher: input.slice(match[1].length),
-            selector: (s) => `${s}[aria-invalid="true"]`,
-          };
-        }
-        return undefined;
-      },
+    // cf. https://github.com/unocss/unocss/pull/1816
+    aria: {
+      invalid: 'invalid="true"',
     },
-  ],
+  },
   shortcuts: {
     spinner: `
       animate-spin
@@ -51,8 +39,7 @@ export default defineConfig({
       disabled:(bg-base-disabled border-base-outline-disabled)
       not-disabled:hover:border-primary
       not-disabled:focus:(border-primary ring-2 ring-primary-outline)
-      aria-invalid:!border-error
-      aria-invalid:focus:(ring-2 !ring-error-outline)
+      aria-invalid:(!border-error focus:(ring-2 !ring-error-outline))
     `,
     // https://ant.design/components/button/
     btn: `
