899b58a636eac587765467859fe7504fc8b24403
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2022-11-27 17:22:13 +0900

    chore: ignore subprojects for linting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,5 +6,5 @@ module.exports = {
     "import/order": ["error", { alphabetize: { order: "asc" } }],
     "sort-imports": ["error", { ignoreDeclarationSort: true }],
   },
-  ignorePatterns: ["build", "dist", ".vercel", "third_party"],
+  ignorePatterns: ["build", "dist", ".vercel", "third_party", "subprojects"],
 };
