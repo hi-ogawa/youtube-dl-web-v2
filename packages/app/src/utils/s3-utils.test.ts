620920ad447b49a54f9a439dcbb599adaa97e804
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-05-14 01:29:13 +0900

    feat: add asset utils

--- packages/app/src/utils/s3-utils.test.ts
+++ packages/app/src/utils/s3-utils.test.ts
@@ -1,10 +1,14 @@
-import { fetch } from "undici";
+import { tinyassert } from "@hiogawa/utils";
+import { Blob, FormData, fetch } from "@remix-run/web-fetch";
 import { assert, beforeAll, describe, expect, it } from "vitest";
 import { z } from "zod";
 import {
+  getAssetDownloadUrl,
+  getAssetUploadPost,
+  listAssets,
   s3GetDownloadUrl,
-  s3GetUploadUrl,
-  s3ListKeys,
+  s3GetUploadPost,
+  s3ListObjects,
   s3ResetBucket,
 } from "./s3-utils";
 
@@ -15,21 +19,26 @@ describe("s3-utils", () => {
 
   it("basic", async () => {
     {
-      const res = await s3ListKeys();
+      const res = await s3ListObjects({});
       expect(res).toMatchInlineSnapshot("[]");
     }
 
     {
-      const uploadUrl = await s3GetUploadUrl("hello.txt");
-      const res = await fetch(uploadUrl, {
-        method: "PUT",
-        body: "hello world",
+      const { url, fields } = await s3GetUploadPost({ Key: "hello.txt" });
+      const formData = new FormData();
+      for (const [k, v] of Object.entries(fields)) {
+        formData.append(k, v);
+      }
+      formData.append("file", new Blob(["hello world"]));
+      const res = await fetch(url, {
+        method: "POST",
+        body: formData,
       });
       assert.ok(res.ok);
     }
 
     {
-      let res = await s3ListKeys();
+      let res = await s3ListObjects({});
       res = z
         .object({
           ETag: z.any(),
@@ -50,10 +59,92 @@ describe("s3-utils", () => {
     }
 
     {
-      const downloadUrl = await s3GetDownloadUrl("hello.txt");
+      const downloadUrl = await s3GetDownloadUrl({
+        Key: "hello.txt",
+        ResponseContentType: "plain/text",
+        ResponseContentDisposition: `attachment; filename="different.txt"`,
+      });
       const res = await fetch(downloadUrl);
       assert.ok(res.ok);
       expect(await res.text()).toMatchInlineSnapshot('"hello world"');
+
+      let headers = Object.fromEntries(res.headers.entries());
+      headers = z
+        .object({
+          "content-length": z.string(),
+          "content-disposition": z.string(),
+          "content-type": z.string(),
+        })
+        .parse(headers);
+      expect(headers).toMatchInlineSnapshot(`
+        {
+          "content-disposition": "attachment; filename=\\"different.txt\\"",
+          "content-length": "11",
+          "content-type": "plain/text",
+        }
+      `);
+    }
+  });
+});
+
+describe("asset-utils", () => {
+  beforeAll(async () => {
+    await s3ResetBucket();
+  });
+
+  it("basic", async () => {
+    {
+      const assets = await listAssets();
+      expect(assets).toMatchInlineSnapshot(`
+        {
+          "assets": [],
+          "nextCursor": undefined,
+        }
+      `);
+    }
+
+    {
+      const { url, fields } = await getAssetUploadPost({
+        sortKey: "abc",
+        filename: "hello.txt",
+        contentType: "plain/txt",
+        extra: { videoId: "D-X0jqkguhs" },
+      });
+      const formData = new FormData();
+      for (const [k, v] of Object.entries(fields)) {
+        formData.append(k, v);
+      }
+      formData.append("file", new Blob(["hello world"]));
+      const res = await fetch(url, {
+        method: "POST",
+        body: formData,
+      });
+      assert.ok(res.ok);
+    }
+
+    {
+      const res = await listAssets();
+      expect(res).toMatchInlineSnapshot(`
+        {
+          "assets": [
+            {
+              "contentType": "plain/txt",
+              "extra": {
+                "videoId": "D-X0jqkguhs",
+              },
+              "filename": "hello.txt",
+              "key": "abc-eyJmaWxlbmFtZSI6ImhlbGxvLnR4dCIsImNvbnRlbnRUeXBlIjoicGxhaW4vdHh0IiwiZXh0cmEiOnsidmlkZW9JZCI6IkQtWDBqcWtndWhzIn19",
+              "sortKey": "abc",
+            },
+          ],
+          "nextCursor": "abc-eyJmaWxlbmFtZSI6ImhlbGxvLnR4dCIsImNvbnRlbnRUeXBlIjoicGxhaW4vdHh0IiwiZXh0cmEiOnsidmlkZW9JZCI6IkQtWDBqcWtndWhzIn19",
+        }
+      `);
+
+      const url = await getAssetDownloadUrl({ key: res.assets[0].key });
+      const download = await fetch(url);
+      tinyassert(download.ok);
+      expect(await download.text()).toMatchInlineSnapshot('"hello world"');
     }
   });
 });
--- packages/app/src/utils/s3-utils.ts
+++ packages/app/src/utils/s3-utils.ts
@@ -1,13 +1,23 @@
-import { execSync } from "node:child_process";
-import { GetObjectCommand, PutObjectCommand, S3 } from "@aws-sdk/client-s3";
+import { spawn } from "node:child_process";
+import {
+  GetObjectCommand,
+  GetObjectCommandInput,
+  ListObjectsV2CommandInput,
+  S3,
+} from "@aws-sdk/client-s3";
+import { createPresignedPost } from "@aws-sdk/s3-presigned-post";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { typedBoolean } from "@hiogawa/utils";
+import { z } from "zod";
 import { serverConfig } from "./config";
 
 // https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/clients/client-s3/classes/s3.html
 // https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/modules/_aws_sdk_s3_request_presigner.html
+// https://github.com/aws/aws-sdk-js-v3/blob/864b34a52aea8dbd52a63239027b4c203c75863c/packages/s3-presigned-post/README.md
+
+// TODO: object lifecycle to auto delete
 
 let s3: S3;
-let bucketArg: { Bucket: string };
 
 export function initializeS3() {
   s3 = new S3({
@@ -17,37 +27,130 @@ export function initializeS3() {
     },
     endpoint: serverConfig.APP_S3_ENDPOINT,
     region: serverConfig.APP_S3_REGION,
-    // sneaky required config https://docs.localstack.cloud/user-guide/integrations/sdks/javascript/
+    // this sneaky config is required for https://docs.localstack.cloud/user-guide/integrations/sdks/javascript/
     forcePathStyle: true,
   });
-
-  bucketArg = { Bucket: serverConfig.APP_S3_BUCKET };
 }
 
-export async function s3ListKeys() {
-  const res = await s3.listObjects(bucketArg);
+export async function s3ListObjects(
+  options: Omit<ListObjectsV2CommandInput, "Bucket">
+) {
+  // https://docs.aws.amazon.com/AmazonS3/latest/userguide/ListingKeysUsingAPIs.html
+  // > List results are always returned in UTF-8 binary order.
+  const res = await s3.listObjectsV2({
+    ...options,
+    Bucket: serverConfig.APP_S3_BUCKET,
+  });
   const objects = res.Contents ?? [];
   return objects;
 }
 
-export async function s3GetDownloadUrl(key: string) {
+export async function s3GetDownloadUrl(
+  options: Omit<GetObjectCommandInput, "Bucket">
+) {
   const command = new GetObjectCommand({
-    ...bucketArg,
-    Key: key,
+    ...options,
+    Bucket: serverConfig.APP_S3_BUCKET,
   });
   const url = await getSignedUrl(s3, command, { expiresIn: 60 * 10 });
   return url;
 }
 
-export async function s3GetUploadUrl(key: string) {
-  const command = new PutObjectCommand({
-    ...bucketArg,
-    Key: key,
+export async function s3GetUploadPost(options: { Key: string }) {
+  const res = await createPresignedPost(s3, {
+    ...options,
+    Bucket: serverConfig.APP_S3_BUCKET,
+    Expires: 60 * 10,
   });
-  const url = await getSignedUrl(s3, command, { expiresIn: 60 * 10 });
-  return url;
+  return res;
 }
 
 export async function s3ResetBucket() {
-  execSync("pnpm -s s3-reset:test");
+  await spawnPromsie("pnpm s3-reset:test");
+}
+
+async function spawnPromsie(command: string) {
+  // exec with inherit io
+  const child = spawn(command, {
+    stdio: "inherit",
+    shell: true,
+  });
+  await new Promise<void>((resolve, reject) => {
+    child.on("close", (code) => (code === 0 ? resolve() : reject()));
+    child.on("error", reject);
+  });
+}
+
+//
+// s3 wrapper for our application needs
+//
+// we encode everything into object key for
+// - timestamp based ordering for listObjects
+// - metadata for videoId, title, etc... (without relying on "object tagging")
+//
+
+const Z_ASSET_CREATE = z.object({
+  sortKey: z.string().regex(/^[0-9a-z]*$/),
+  filename: z.string(),
+  contentType: z.string(),
+  extra: z.record(z.string()),
+});
+
+type AssetCreate = z.infer<typeof Z_ASSET_CREATE>;
+
+type Asset = { key: string } & AssetCreate;
+
+function encodeString(s: string) {
+  return Buffer.from(s, "ascii").toString("base64url");
+}
+
+function decodeString(s: string) {
+  return Buffer.from(s, "base64url").toString("ascii");
+}
+
+function splitFirst(s: string, sep: string): [string, string] {
+  const i = s.indexOf(sep);
+  if (i === -1) {
+    return [s, ""];
+  }
+  return [s.slice(0, i), s.slice(i + sep.length)];
+}
+
+function encodeAssetKey(asset: AssetCreate): string {
+  // parse will move `sortKey` to front
+  const { sortKey, ...rest } = asset;
+  const restString = encodeString(JSON.stringify(rest));
+  return sortKey + "-" + restString;
+}
+
+function decodeAssetKey(key: string): AssetCreate {
+  const [sortKey, restString] = splitFirst(key, "-");
+  const rest = JSON.parse(decodeString(restString));
+  return Z_ASSET_CREATE.parse({ sortKey, ...rest });
+}
+
+export async function getAssetUploadPost(asset: AssetCreate) {
+  const Key = encodeAssetKey(asset);
+  return s3GetUploadPost({ Key });
+}
+
+export async function getAssetDownloadUrl(asset: Pick<Asset, "key">) {
+  const { filename, contentType } = decodeAssetKey(asset.key);
+  return s3GetDownloadUrl({
+    Key: asset.key,
+    ResponseContentType: contentType,
+    ResponseContentDisposition: `attachment; filename="${filename}"`,
+  });
+}
+
+export async function listAssets(
+  cursor?: string
+): Promise<{ assets: Asset[]; nextCursor?: string }> {
+  const objects = await s3ListObjects({
+    StartAfter: cursor,
+    MaxKeys: 5,
+  });
+  const keys = objects.map((o) => o.Key).filter(typedBoolean); // TODO: when is `Key` undefined?
+  const assets = keys.map((key) => ({ key, ...decodeAssetKey(key) }));
+  return { assets, nextCursor: keys.at(-1) };
 }
