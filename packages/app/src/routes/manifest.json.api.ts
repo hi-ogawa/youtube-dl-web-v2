3ebf07b5b5f9d92d3f49ecd489ce20712523bc7c
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-05-25 09:38:06 +0900

    chore: cache manifest.json

--- packages/app/src/routes/manifest.json.api.ts
+++ packages/app/src/routes/manifest.json.api.ts
@@ -1,6 +1,12 @@
 import ICON192_URL from "../assets/icon-192.png?url";
 import ICON512_URL from "../assets/icon-512.png?url";
-import { json } from "../utils/handler-utils";
+import { PRESET_HEADERS, json } from "../utils/handler-utils";
+
+export function get() {
+  return json(MANIFEST_JSON, {
+    headers: PRESET_HEADERS.CACHE_CDN,
+  });
+}
 
 export const SHARE_TARGET_PARAMS = {
   title: "share-target-title",
@@ -8,33 +14,31 @@ export const SHARE_TARGET_PARAMS = {
   text: "share-target-text",
 };
 
-export function get() {
-  return json({
-    short_name: "Youtube DL Web",
-    name: "Youtube DL Web",
-    icons: [
-      {
-        src: ICON192_URL,
-        type: "image/png",
-        sizes: "192x192",
-      },
-      {
-        src: ICON512_URL,
-        type: "image/png",
-        sizes: "512x512",
-      },
-    ],
-    id: "/",
-    start_url: "/",
-    scope: "/",
-    theme_color: "#FFFFFF",
-    background_color: "#FFFFFF",
-    display: "standalone",
-    share_target: {
-      action: "/",
-      method: "GET",
-      enctype: "application/x-www-form-urlencoded",
-      params: SHARE_TARGET_PARAMS,
+const MANIFEST_JSON = {
+  short_name: "Youtube DL Web",
+  name: "Youtube DL Web",
+  icons: [
+    {
+      src: ICON192_URL,
+      type: "image/png",
+      sizes: "192x192",
     },
-  });
-}
+    {
+      src: ICON512_URL,
+      type: "image/png",
+      sizes: "512x512",
+    },
+  ],
+  id: "/",
+  start_url: "/",
+  scope: "/",
+  theme_color: "#FFFFFF",
+  background_color: "#FFFFFF",
+  display: "standalone",
+  share_target: {
+    action: "/",
+    method: "GET",
+    enctype: "application/x-www-form-urlencoded",
+    params: SHARE_TARGET_PARAMS,
+  },
+};
