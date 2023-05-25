5ba06251359d3bfcffb49272985bb7ad3e6bd0cc
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-05-25 20:51:36 +0900

    chore: add captcha config

--- packages/app/src/utils/config.ts
+++ packages/app/src/utils/config.ts
@@ -5,11 +5,16 @@ const NODE_ENV = process.env["NODE_ENV"] ?? "development";
 
 // prettier-ignore
 const Z_SERVER_CONFIG = z.object({
+  // s3
   APP_S3_ENDPOINT: z.string().default("http://localhost:4566"),
   APP_S3_ACCESS_KEY_ID: z.string().default("na"),
   APP_S3_SECRET_ACCESS_KEY: z.string().default("na"),
   APP_S3_REGION: z.string().default("us-east-1"),
   APP_S3_BUCKET: z.string().default(NODE_ENV),
+
+  // captcha
+  APP_CAPTCHA_SITE_KEY: z.string().optional(),
+  APP_CAPTCHA_SECRET_KEY: z.string().optional(),
 });
 
 export let serverConfig: z.infer<typeof Z_SERVER_CONFIG>;
