837116acd78eefbdaffdbc75df10e9c6c1136976
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-06-21 17:49:55 +0900

    refactor: replace rakkasjs with vite-glob-routes

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -1,3 +1,4 @@
+import { viteGlobRoutes } from "@hiogawa/vite-glob-routes";
 import rakkas from "rakkasjs/vite-plugin";
 import unocss from "unocss/vite";
 import { defineConfig } from "vite";
@@ -7,5 +8,9 @@ export default defineConfig({
   build: {
     sourcemap: true,
   },
-  plugins: [unocss(), rakkas({ adapter: "vercel" })],
+  plugins: [
+    unocss(),
+    viteGlobRoutes({ root: "/src/routes" }),
+    rakkas({ adapter: "vercel" }),
+  ],
 });
