4e94a50d517848a95cbbbc8de2092800e78b05d1
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-07-01 11:44:17 +0900

    chore: update tiny-rpc

--- packages/app/src/trpc/hattip.ts
+++ packages/app/src/trpc/hattip.ts
@@ -1,5 +1,6 @@
 import { RequestHandler } from "@hattip/compose";
 import { createTinyRpcHandler } from "@hiogawa/tiny-rpc";
+import { SpanStatusCode, trace } from "@opentelemetry/api";
 import { RPC_ENDPOINT } from "./client";
 import { rpcRoutes } from "./server";
 
@@ -7,6 +8,14 @@ export function rpcHandler(): RequestHandler {
   const handler = createTinyRpcHandler({
     endpoint: RPC_ENDPOINT,
     routes: rpcRoutes,
+    onError(e) {
+      console.error(e);
+      const span = trace.getActiveSpan();
+      if (span) {
+        span.setStatus({ code: SpanStatusCode.ERROR });
+        span.recordException(e instanceof Error ? e : new Error());
+      }
+    },
   });
   return async (ctx) => {
     if (ctx.url.pathname.startsWith(RPC_ENDPOINT)) {
