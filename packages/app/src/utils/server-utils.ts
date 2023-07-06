65acd6b55601971de0ba8c0dad5e2d7c2b8fd39d
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-07-06 12:58:18 +0900

    chore: remove s3-utils

--- packages/app/src/utils/server-utils.ts
+++ packages/app/src/utils/server-utils.ts
@@ -2,12 +2,10 @@ import { RequestHandler } from "@hattip/compose";
 import { once } from "@hiogawa/utils";
 import { initializeConfig } from "./config";
 import { finalizeOtel, initializeOtel } from "./otel-utils";
-import { initializeS3 } from "./s3-utils";
 
 export async function initializeServer() {
   await initializeOtel();
   initializeConfig();
-  initializeS3();
 }
 
 export async function finalizeServer() {
