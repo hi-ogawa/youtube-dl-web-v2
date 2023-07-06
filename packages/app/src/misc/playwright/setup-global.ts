32fe9ce6a62a08c77472298986015da4c1919ac6
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-07-06 13:34:26 +0900

    test: fix e2e

--- packages/app/src/misc/playwright/setup-global.ts
+++
@@ -1,8 +0,0 @@
-import { exec } from "node:child_process";
-import { promisify } from "node:util";
-
-const execPromsie = promisify(exec);
-
-export default async () => {
-  await execPromsie("make localstack/reset/test");
-};
