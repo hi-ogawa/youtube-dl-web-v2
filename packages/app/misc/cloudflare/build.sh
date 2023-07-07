12b2b5a957ad9f44f132bd6836b9feab0098d111
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-07-07 10:07:06 +0900

    build: tweak esbuild

--- packages/app/misc/cloudflare/build.sh
+++ packages/app/misc/cloudflare/build.sh
@@ -18,10 +18,11 @@ mkdir -p dist/cloudflare/bucket
 # dist/cloudflare/index.js
 ESBUILD_OPTS=(--minify --sourcemap=external)
 if [ -n "${DEBUG:-}" ]; then
+  # better local preview stacktrace (workerd doesn't support sourcemap anyway?)
   ESBUILD_OPTS=(--sourcemap=inline)
 fi
 esbuild dist/server/index.js "${ESBUILD_OPTS[@]}" --outfile=dist/cloudflare/index.js --metafile=dist/esbuild-metafile.json \
-  --bundle --minify --format=esm --platform=browser --external:__STATIC_CONTENT_MANIFEST
+  --bundle --format=esm --platform=browser --external:__STATIC_CONTENT_MANIFEST
 
 # dist/cloudflare/bucket
 cp -r dist/client/assets dist/cloudflare/bucket/assets
