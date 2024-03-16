55fcf300ac99ca257839f2e35e164e96fe710d89
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-03-16 11:16:32 +0900

    fix: fix youtube api (#88)

--- packages/app/src/utils/youtube-utils.ts
+++ packages/app/src/utils/youtube-utils.ts
@@ -109,7 +109,7 @@ export async function fetchVideoInfoRaw(videoId: string): Promise<any> {
       context: {
         client: {
           clientName: "ANDROID",
-          clientVersion: "17.31.35",
+          clientVersion: "18.11.34",
           androidSdkVersion: 30,
           hl: "en",
           timeZone: "UTC",
@@ -119,9 +119,9 @@ export async function fetchVideoInfoRaw(videoId: string): Promise<any> {
     }),
     headers: {
       "X-YouTube-Client-Name": "3",
-      "X-YouTube-Client-Version": "17.31.35",
+      "X-YouTube-Client-Version": "18.11.34",
       "Origin": "https://www.youtube.com",
-      "User-Agent": "com.google.android.youtube/17.31.35 (Linux; U; Android 11) gzip",
+      "User-Agent": "com.google.android.youtube/18.11.34 (Linux; U; Android 11) gzip",
       "content-type": "application/json",
     }
   });
