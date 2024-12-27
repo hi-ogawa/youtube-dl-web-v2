758479796ca4787c4948e3312fa6a084c1f0b7d1
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-12-27 13:53:11 +0900

    wip

--- packages/app/src/utils/youtube-utils.ts
+++ packages/app/src/utils/youtube-utils.ts
@@ -100,7 +100,6 @@ export async function fetchVideoInfo(videoId: string): Promise<VideoInfo> {
 }
 
 // cf. https://gist.github.com/hi-ogawa/23f6d0b212f51c2b1b255339c642e9b9
-// https://github.com/yt-dlp/yt-dlp/blob/0b6b7742c2e7f2a1fcb0b54ef3dd484bab404b3f/yt_dlp/extractor/youtube.py#L202
 export async function fetchVideoInfoRaw(videoId: string): Promise<any> {
   // prettier-ignore
   const res = await fetch("https://www.youtube.com/youtubei/v1/player", {
@@ -109,23 +108,38 @@ export async function fetchVideoInfoRaw(videoId: string): Promise<any> {
       videoId,
       context: {
         client: {
-          'clientName': 'IOS',
-          'clientVersion': '19.45.4',
-          'deviceMake': 'Apple',
-          'deviceModel': 'iPhone16,2',
-          'userAgent': 'com.google.ios.youtube/19.45.4 (iPhone16,2; U; CPU iOS 18_1_0 like Mac OS X;)',
-          'osName': 'iPhone',
-          'osVersion': '18.1.0.22B83',
+          clientName: "IOS",
+          clientVersion: "19.45.4",
+          deviceMake: "Apple",
+          deviceModel: "iPhone16,2",
+          userAgent:
+            "com.google.ios.youtube/19.45.4 (iPhone16,2; U; CPU iOS 18_1_0 like Mac OS X;)",
+          osName: "iPhone",
+          osVersion: "18.1.0.22B83",
+          hl: "en",
+          timeZone: "UTC",
+          utcOffsetMinutes: 0,
         },
       },
+      playbackContext: {
+        contentPlaybackContext: {
+          html5Preference: "HTML5_PREF_WANTS",
+          signatureTimestamp: 20073,
+        },
+      },
+      contentCheckOk: true,
+      racyCheckOk: true,
     }),
     headers: {
       "X-YouTube-Client-Name": "5",
       "X-YouTube-Client-Version": "19.45.4",
-      "Origin": "https://www.youtube.com",
-      "User-Agent": "com.google.ios.youtube/19.45.4 (iPhone16,2; U; CPU iOS 18_1_0 like Mac OS X;)",
+      Origin: "https://www.youtube.com",
+      "User-Agent":
+        "com.google.ios.youtube/19.45.4 (iPhone16,2; U; CPU iOS 18_1_0 like Mac OS X;)",
       "content-type": "application/json",
-    }
+      "X-Goog-Visitor-Id":
+        "CgtwU3N6UXVjakdWbyi94bi7BjIKCgJKUBIEGgAgUQ%3D%3D",
+    },
   });
   tinyassert(res.ok);
   tinyassert(res.headers.get("content-type")?.startsWith("application/json"));
